5c004ccf65465d9a656c9af50fbd397e9c1333cc
Natanael Log <natte.log@gmail.com>
2015-03-02 20:58:21 +0100

    Added some sections

--- index.php
+++ index.php
@@ -1,37 +1,53 @@
-<html ng-app="wedding">
+<html>
     <head>  
         <title>Sara och Natanael</title>
         <link rel="stylesheet" href="lib/bootstrap.min.css">
         <link rel="stylesheet" href="lib/font-awesome.css">
         <link rel="stylesheet" href="style.css">
-        <script src="../lib/angular.min.js"></script>
-        <script src="app.js"></script>
+        <script src="//use.typekit.net/piv1mvd.js"></script>
+<script>try{Typekit.load();}catch(e){}</script>
         <meta charset="utf-8">
     </head>
-    <body ng-controller="globalController">  
+    <body>  
+	    
         <div class="wrapper">
-	        
-		<!-- NAVBAR -->
-	    <div class="nav">
-		    <h3>NAVBAR</h3>
-	    </div>
 	    
-		<!-- HEADER -->
-	    <div class="header">
-		    <h3>HEADER</h3>
-	    </div>
+			<!-- HEADER -->
+		    <div class="header tree-img bg-cover">
+			    <span class="tk-lust header-text">Sara<br>&<br>Natanael</span>
+			</div>
 	    
-		<!-- CONTENT -->
-		<div class="content">
-			<h3>CONTENT</h3>
-		</div>
-	
-		<div class="push"></div>
-        </div>
-        
-		<!-- FOOTER -->
-		<div class="footer">
-			<h3>FOOTER</h3>
-		</div>
+			<!-- CONTENT -->
+			<div class="content">
+				
+				<!-- ROAD DIRECTION SECTION -->
+				<section>
+					<div class="section-content">
+						<h1>VÄGBESKRIVNING</h1>
+						<p>Åk norrut.</p>
+					</div>
+				</section>
+				
+				
+				<div class="section-split fill">
+					<img src="img/kissing-img.jpg">
+				</div>
+				
+				<!-- WISH LIST SECTION -->
+				<section>
+					<div class="section-content">
+						<h1>ÖNSKELISA</h1>
+						<p>Pengar.</p>
+					</div>
+				</section>
+				
+				<!-- FOOTER -->
+				<footer>
+					<h4>FOOTER</h4>
+				</footer>
+			
+			</div>
+			
+    	</div>
     </body>
 </html>
