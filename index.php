c1cc1ffaee66949f208b7d24e1595b20c44fa19f
Natanael Log <natte.log@gmail.com>
2015-03-21 13:13:59 +0100

    Worked with the new layout

--- index.php
+++ index.php
@@ -13,10 +13,19 @@
         <div class="wrapper">
 	    
 			<!-- HEADER -->
-		    <div class="header tree-img bg-cover">
-			    <span class="header-arc">15 AUGUSTI 2015</span>
-			    <span class="tk-lust header-text">Sara<br>&<br>Natanael</span>
-			    <span class="header-arc">VIBY, HALLSBERG</span>
+		    <div class="header tree-img bg-cover tk-grafolita-script">
+			    <span>
+			    	<div class="heart-svg"><svg version="1.1" id="Lager_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve">
+<path d="M991,148.9C949,22.8,779.9,46.4,689.1,95.5C581.6,153.8,500,273.4,475.9,396.2C399.1,225.6,153.6,22.3,35.2,233.6
+	C-42.9,373,21.5,539.6,106.9,658c92.9,128.8,231.2,238.3,383.9,285.5c6,1.8,10.2-1.3,12-5.6c1.2-0.4,2.5-0.8,3.7-1.5
+	c31.7-18.2,51.7-50.5,74.6-78.1c32.6-39.4,70.3-75.4,105.3-112.6c74.1-78.6,146.2-158.9,208.8-247.1
+	C962.9,403.1,1030.8,268.5,991,148.9z"/>
+</svg></div>
+				    <h2>Vi ska gifta oss!</h2>
+				    <h1>Sara Danell<br>Natanael Log</h1>
+				    <h2>femtonde augusti klockan fjorton</h2>
+			    </span>
 			</div>
 	    
 			<!-- CONTENT -->
@@ -24,9 +33,17 @@
 				
 				<!-- ROAD DIRECTION SECTION -->
 				<section>
-					<div class="section-content">
-						<h1>VÄGBESKRIVNING</h1>
-						<p>Åk norrut.</p>
+					<div class="section-column">
+						<div class="section-content">
+							<h1>VÄGBESKRIVNING</h1>
+							<p>Åk norrut.</p>
+						</div>
+					</div>
+					<div class="section-column">
+						<div class="section-content">
+							<h1>VÄGBESKRIVNING</h1>
+							<p>Åk norrut.</p>
+						</div>
 					</div>
 				</section>
 				
