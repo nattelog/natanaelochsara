9a399e2a017adc30cd07fff3666c0f4c68ee7fc9
Natanael Log <natte.log@gmail.com>
2015-03-24 18:46:23 +0100

    Changed in html

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
-<html ng-app="wedding">
+<!DOCTYPE html>
+<html>
     <head>  
         <title>Sara och Natanael</title>
         <link rel="stylesheet" href="lib/bootstrap.min.css">
         <link rel="stylesheet" href="lib/font-awesome.css">
         <link rel="stylesheet" href="style.css">
         <script src="../lib/angular.min.js"></script>
-        <script src="app.js"></script>
         <meta charset="utf-8">
     </head>
-    <body ng-controller="globalController">  
+    <body>  
         <div class="wrapper">
             <div class="temp-info">
                 <span class="fa-stack fa-3x">
